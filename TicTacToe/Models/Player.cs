7f262ae2c912178dbe8eb590ed392b39c38afc5d
Gintasss <svedgintas@gmail.com>
2020-01-18 20:03:22 +0200

    Added two models.

---
+++ TicTacToe/Models/Player.cs
@@ -0,0 +1,45 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe.Models
+{
+    // Player model that will hold information about a single player
+    // (either a human player or a computer player)
+    public class Player
+    {
+        public char Sign => Container.Tag.ToString()[0];
+        public Image SignImage => Container.Image;
+
+        private PictureBox Container;
+        // Is to keep reference for a player name when a gamemode changes.
+        private Label NameLabel;
+
+        public Player(PictureBox container, Label playerNameLabel)
+        {
+            Container = container;
+            NameLabel = playerNameLabel;
+        }
+
+        // Swap player when sign changes.
+        public void Swap(Player player)
+        {
+            var tempImage = (Image)Container.Image.Clone();
+            var tempSign = Container.Tag;
+
+            Container.Image = (Image)player.SignImage.Clone();
+            Container.Tag = player.Sign;
+
+            player.Container.Image = tempImage;
+            player.Container.Tag = tempSign;
+        }
+
+        // Congratulate player when he wins the game.
+        public void Congratulate()
+        {
+            MessageBox.Show(NameLabel.Text + Constants.GameState.Win);
+        }
+
+
+
+    }
+}
---
+++ TicTacToe/Models/TIle.cs
@@ -0,0 +1,29 @@
+﻿using System.Drawing;
+using System.Windows.Forms;
+
+namespace TicTacToe.Models
+{
+    // Model is responsible for holding single tile information.
+    public class Tile
+    {
+        public PictureBox Container { get; private set; }
+        public char Sign => Container.Tag.ToString()[0];
+        public bool IsTileEnabled => Container.Enabled;
+
+        public Tile(PictureBox container)
+        {
+            Container = container;
+        }
+
+        // Set tile by player's information.
+        public void SetTile(Player player)
+        {
+            Container.Tag = player.Sign;
+            Container.Image = (Image)player.SignImage.Clone();
+            Container.Enabled = false;
+        }
+
+
+
+    }
+}
